e09943becb59ab0baaab7b800002acd7b2add5f6
jeftejb <jeftesambangojb@gmail.com>
2024-03-30 05:42:26 +0100

    commit1 de 29/3

---
+++ index.ts
@@ -0,0 +1,63 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+//const nodemailer = require("nodemailer");
+
+
+const app = express();
+const port = 8000 ;
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://jeftesambangojb:oepd48XXfcHo0yeJ@vandjabesmart.kc1ckt5.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+    console.log("Conexao bem sucedida com o mongo db")
+}).catch((error:any)=>{
+console.log("Algo deu errado ao fazer a conexao", error)
+})
+
+app.listen(port, ()=>{
+    console.log("O servidor esta rodando na PORTA:3000")
+})
+
+
+// rotas 
+import Funcionarios from "./models/funcionarios"
+import Salarios from "./models/Salarios"
+
+app.post("/funcionario", async(req:any, res:any) =>{
+  const email:any = req.body;
+    try {
+      
+        const findFuncionario =  await Funcionarios.findOne({ email: email?.email}).populate({
+          path: "salarios",
+          model: Salarios,
+        });
+
+        return res.json(findFuncionario);
+      
+
+      } catch (error: any) {
+       // throw new Error(`Failed to fetch user: ${error.message}`);
+      }
+})
+
+app.post("/presenca", async(req:any, res:any)=>{
+    const id:string = new mongoose.Types.ObjectId(req.body.id)
+    const dadosPresenca:any = {data:req.body.data, hora:req.body.hora}
+    try {
+        
+        await Funcionarios.findOneAndUpdate({_id:id}, {
+            $push : {presencas:dadosPresenca},
+            
+          });
+          return res.status(200).json({mesagem:"Assinatura feita com sucesso"});
+    } catch (error:any) {
+        return{mesagem:"error"}
+        
+    }
+
+})
---
+++ models/funcionarios.ts
@@ -0,0 +1,91 @@
+import mongoose from "mongoose";
+
+const funcionariosSchema = new mongoose.Schema({
+
+  imagem: String,
+  bi1: String,
+  bi2:String,
+id:{type:String},
+  email:{
+    type:String
+  },
+
+  nomeCompleto: {
+    type: String,
+   
+  },
+
+
+  turmas:[ 
+    {
+    type: String,
+  }
+],
+
+  dataNascimento: {
+    type: String,
+
+  },
+
+  telefone: {
+    type: Number,
+    
+  },
+
+  sexo: {
+    type: String,
+    
+  },
+
+ 
+  onboarded: {
+    type: Boolean,
+    default: false,
+  },
+
+  admin:{
+    type:Boolean,
+    default:false,
+    require:true
+  },
+  salarios: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Salarios",
+    },
+  ],
+
+ funcoes : [{
+    type:String
+ }],
+
+ linkAula:[
+  {
+    link:{type:String},
+    nivel:{type:String}
+  }
+],
+
+ horario:[
+  {
+    periodo:{type:String},
+    hora:{type:String}
+  }
+],
+presencas:[
+    {
+      data:{type:String},
+      hora:{type:String}
+    }
+  ],
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+ 
+});
+
+const Funcionarios = mongoose.models.Funcionarios || mongoose.model("Funcionarios", funcionariosSchema);
+
+export default Funcionarios;
