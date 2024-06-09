e836899ba7d3c0e2fa96e2b86e52f3cfc1ddab21
jeftejb <jeftesambangojb@gmail.com>
2024-06-09 03:22:44 +0000

    act5  8.6.2024

--- src/index.ts
+++ src/index.ts
@@ -25,8 +25,8 @@ app.listen(port, ()=>{
 })
 
 
-import Funcionarios from "../source/models/funcionarios"
-import Salarios from "../source/models/Salarios"
+import Funcionarios from "./models/funcionarios"
+import Salarios from "./models/Salarios"
 
 app.post("/funcionario", async(req:any, res:any) =>{
   const email:any = req.body;
